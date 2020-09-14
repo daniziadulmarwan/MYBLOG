8dd3b642b31175e805f66e72a21b3379f7685f1f
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2020-09-15 01:14:42 +0700

    add some code

---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <label>label input</label>
+      <input placeholder="" />
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import { Input } from "../../components";
+import "./register.scss";
 
 const Register = () => {
   return (
-    <div>
-      <p>Register Page</p>
+    <div className="main-page">
+      <div className="right">
+        <img src={RegisterBg} className="bg-image" alt="image" />
+      </div>
+      <div className="left">
+        <p>Register</p>
+        <Input />
+      </div>
     </div>
   );
 };
